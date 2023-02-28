652952abf672a6b1ae2e96223d423d0cdcd7af05
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2023-02-28 21:05:56 +0000

    Main section setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MainModule } from './main/main.module';
 
 import { AppComponent } from './app.component';
 
+
 const routes: Routes = [
   {path:'', pathMatch:'full', redirectTo:'posts'}
 ];
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,13 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 
+ 
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostFormComponent } from './post-form/post-form.component';
+
 const routes: Routes = [
   {path: 'posts', component: MainComponent}
 ];
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    PostDetailsComponent,
+    PostListComponent,
+    PostFormComponent
   ],
   imports: [
     CommonModule,
