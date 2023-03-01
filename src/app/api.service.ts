273ea8ffaff215df67864c818586577061ef8e25
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2023-03-01 14:12:34 +0100

    Data successfully fetched from the backend

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,6 +8,11 @@ export class ApiService {
 
   baseUrl = 'http://127.0.0.1:8000/api/posts/';
 
+  headers = new HttpHeaders({
+   'Content-Type':'application/json',
+   Authorization:'Token 32f32230169bbb7f275e3002637ba59d4d0a9b2a'
+  });
+
   // private _posts = ['There is still hope','Let us have faith'];
 
   constructor(
@@ -15,7 +20,7 @@ export class ApiService {
   ) { }
 
   getPosts(){
-    return this.httpClient.get(this.baseUrl);
+    return this.httpClient.get(this.baseUrl, {headers: this.headers});
     
     // return this._posts;
   }
